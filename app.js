d6d7d080ae4c0ccffda1b24141b02434d1cb30a1
Saundie Weiss <saundieweiss@gmail.com>
2016-03-27 20:18:15 -0600

    Removed process.env.PORT

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ if (app.get('env') === 'development') {
 // });
 
 
-app.listen(process.ENV.PORT || 3000, function () {
+app.listen(3000, function () {
   console.log('Listening on port 3000...');
 });
